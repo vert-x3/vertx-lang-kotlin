e7dceb1cf1204637872aafb3df997da04980fea1
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-11-08 21:11:19 +0300

    Reformat tabs to 2

--- vertx-lang-kotlin-compiler/src/test/resources/singleVerticle.kt
+++ vertx-lang-kotlin-compiler/src/test/resources/singleVerticle.kt
@@ -1,19 +1,19 @@
 import io.vertx.core.*
 
 class V1 : Verticle {
-    private var vertx: Vertx? = null
+  private var vertx: Vertx? = null
 
-    override fun init(vertx: Vertx?, context: Context?) {
-        this.vertx = vertx
-    }
+  override fun init(vertx: Vertx?, context: Context?) {
+    this.vertx = vertx
+  }
 
-    override fun start(startFuture: Future<Void>?) {
-        startFuture?.complete()
-    }
+  override fun start(startFuture: Future<Void>?) {
+    startFuture?.complete()
+  }
 
-    override fun stop(stopFuture: Future<Void>?) {
-        stopFuture?.complete()
-    }
+  override fun stop(stopFuture: Future<Void>?) {
+    stopFuture?.complete()
+  }
 
-    override fun getVertx() = vertx
+  override fun getVertx() = vertx
 }
