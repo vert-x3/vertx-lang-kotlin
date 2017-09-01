3284966264561ec87b7d898d143fdaeda11ddcb7
Julien Viet <julien@julienviet.com>
2017-09-01 08:44:09 +0200

    Add event bus tests

--- vertx-lang-kotlin-coroutine/src/test/kotlin/io/vertx/kotlin/coroutines/EventBusTest.kt
+++ vertx-lang-kotlin-coroutine/src/test/kotlin/io/vertx/kotlin/coroutines/EventBusTest.kt
@@ -1,6 +1,9 @@
 package io.vertx.kotlin.coroutines
 
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.Message
+import io.vertx.core.eventbus.ReplyException
+import io.vertx.core.eventbus.ReplyFailure
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import org.junit.After
@@ -45,6 +48,65 @@ class EventBusTest {
     }
     vertx.setTimer(50) {
       consumer.unregister()
+    }
+  }
+
+  @Test
+  fun testReply(testContext: TestContext) {
+    val bus = vertx.eventBus()
+    val consumer = bus.consumer<Int>("the-address")
+    val channel = toChannel(vertx, consumer.bodyStream())
+    val async = testContext.async()
+    bus.consumer<Int>("another-address") { msg ->
+      val v = msg.body()
+      if (v < 5) {
+        println("replying")
+        msg.reply(4)
+      } else {
+        println("ending")
+        msg.reply(null)
+      }
+    }
+    vertx.runCoroutine {
+      var count = 0
+      for (msg in channel) {
+        val reply = asyncResult<Message<Int?>> {
+          bus.send("another-address", msg, it);
+        }
+        val v = reply.body()
+        if (v == null) {
+          break
+        } else {
+          count += v
+        }
+      }
+      async.complete()
+    }
+    for (index in listOf(0, 1, 2, 3, 4, 5)) {
+      bus.send("the-address", index)
+    }
+  }
+
+  @Test
+  fun testReplyFailure(testContext: TestContext) {
+    val bus = vertx.eventBus()
+    val async = testContext.async()
+    bus.consumer<Int>("the-address") { msg ->
+      msg.fail(5, "it-failed")
+    }
+    vertx.runCoroutine {
+      try {
+        val reply = asyncResult<Message<Int?>> {
+          bus.send("the-address", "the-body", it);
+        }
+      } catch(e: Exception) {
+        testContext.assertTrue(e is ReplyException)
+        val err : ReplyException = e as ReplyException
+        testContext.assertEquals(5, err.failureCode())
+        testContext.assertEquals(ReplyFailure.RECIPIENT_FAILURE, err.failureType())
+        testContext.assertEquals("it-failed", err.message)
+        async.complete()
+      }
     }
   }
 }
