f8cfc908335773b876b4aa4ded14f1d101445298
Julien Viet <julien@julienviet.com>
2017-10-25 17:43:14 +0200

    Added a few test to check Vert.x interoperability with coroutines building blocks - fixes #39

--- vertx-lang-kotlin-coroutines/src/test/kotlin/io/vertx/kotlin/coroutines/CoroutineContextTest.kt
+++ vertx-lang-kotlin-coroutines/src/test/kotlin/io/vertx/kotlin/coroutines/CoroutineContextTest.kt
@@ -6,11 +6,18 @@ import io.vertx.core.DeploymentOptions
 import io.vertx.core.Vertx
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.sync.Mutex
+import kotlinx.coroutines.experimental.sync.withLock
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
 
 /**
@@ -82,6 +89,31 @@ class CoroutineContextTest {
     })
   }
 
+  @Test
+  fun testUnconfined(testContext: TestContext) {
+    val async = testContext.async()
+    val current = Thread.currentThread()
+    launch(Unconfined) {
+      testContext.assertEquals(current, Thread.currentThread())
+      awaitEvent<Long> { vertx.setTimer(10, it) }
+      testContext.assertTrue(Context.isOnEventLoopThread())
+      async.complete()
+    }
+  }
+
+  @Test
+  fun testJoin(testContext: TestContext) {
+    val done = AtomicBoolean()
+    val job = launch(vertx.dispatcher()) {
+      awaitEvent<Long> { vertx.setTimer(500, it) }
+      done.set(true)
+    }
+    runBlocking {
+      job.join()
+      testContext.assertTrue(done.get())
+    }
+  }
+
   suspend fun runTest(testContext: TestContext) {
     testContext.assertTrue(Context.isOnVertxThread())
     val ctx = Vertx.currentContext()
@@ -92,5 +124,57 @@ class CoroutineContextTest {
     testContext.assertEquals(id, a.get())
     testContext.assertTrue(Context.isOnEventLoopThread())
     testContext.assertEquals(ctx, Vertx.currentContext())
+  }
+
+  @Test
+  fun testMutex(testContext: TestContext) {
+    val mutex = Mutex()
+    val dispatcher = vertx.dispatcher()
+    val times = 10
+    val latch = testContext.async(times)
+    for (i in 0..times) {
+      launch(dispatcher) {
+        mutex.withLock {
+          testContext.assertTrue(Context.isOnVertxThread())
+          latch.countDown()
+        }
+      }
+    }
+    latch.await(5000)
+  }
+
+  @Test
+  fun testDelay(testContext: TestContext) {
+    val latch = testContext.async()
+    launch(vertx.dispatcher()) {
+      val ctx = Vertx.currentContext()
+      delay(100)
+      testContext.assertEquals(ctx, Vertx.currentContext())
+      latch.complete()
+    }
+  }
+
+  @Test
+  fun testPeriodic(testContext: TestContext) {
+    val count = AtomicInteger()
+    val interrupted = AtomicBoolean()
+    val job = launch(vertx.dispatcher()) {
+      val ctx = Vertx.currentContext()
+      try {
+        testContext.assertEquals(ctx, Vertx.currentContext())
+        repeat(1000) {
+          count.incrementAndGet()
+          delay(10)
+        }
+      } catch (e: Exception) {
+        testContext.assertEquals(ctx, Vertx.currentContext())
+        interrupted.set(true)
+        throw e;
+      }
+    }
+    Thread.sleep(100)
+    job.cancel()
+    testContext.assertTrue(count.get() > 5)
+    testContext.assertTrue(interrupted.get())
   }
 }
