11f76394966712b354dcb292c571ffda226ad42c
okou19900722 <ilayan19900722@gmail.com>
2018-06-09 19:29:58 +0800

    Add a Codegen Module to Add Coroutine Extensions to VertxGen

---
+++ vertx-lang-kotlin-coroutines-codegen/src/main/kotlin/io/vertx/kotlin/coroutines/KotlinCoroutineGeneratorLoader.kt
@@ -0,0 +1,13 @@
+package io.vertx.kotlin.coroutines
+
+import io.vertx.codegen.Generator
+import io.vertx.codegen.GeneratorLoader
+import java.util.stream.Stream
+import javax.annotation.processing.ProcessingEnvironment
+
+class KotlinCoroutineGeneratorLoader : GeneratorLoader {
+  override fun loadGenerators(processingEnv: ProcessingEnvironment?): Stream<Generator<*>> {
+    return Stream.of(KotlinCoroutineGenerator())
+  }
+
+}
