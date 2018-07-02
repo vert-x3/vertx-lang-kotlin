3729b2aa4d270a18af17b1ac4c721ac061d22be8
Julien Viet <julien@julienviet.com>
2018-07-02 14:47:32 +0200

    Doc fixing

--- vertx-lang-kotlin/src/main/docoverride/docoverride/verticles/configuration/package-info.java
+++ vertx-lang-kotlin/src/main/docoverride/docoverride/verticles/configuration/package-info.java
@@ -15,46 +15,6 @@
  */
 
 /**
- * === Passing configuration to a verticle
- *
- * Configuration in the form of JSON can be passed to a verticle at deployment time:
- *
- * [source,javascript]
- * ----
- * var config = {
- *  "name" : "tim",
- *  "directory" : "/blah"
- * };
- * var options = {
- *  "config" : config
- * };
- * vertx.deployVerticle("MyOrderProcessorVerticle.js", options);
- * ----
- *
- * This configuration is then available via the {@link io.vertx.core.Context} object. The configuration is returned as a
- * JavaScript object so you can retrieve data as follows:
- *
- * [source,javascript]
- * ----
- * // The context object can be retrieve either using:
- * console.log("Configuration: " + Vertx.currentContext().config().name);
- * // or:
- * console.log("Configuration: " + vertx.getOrCreateContext().config().name);
- * ----
- *
- * NOTE: The access to the `context` object is made either using {@link io.vertx.core.Vertx#currentContext()} or using
- * {@link io.vertx.core.Vertx#getOrCreateContext()}.
- *
- * === Accessing environment variables in a Verticle
- *
- * Environment variables and system properties are accessible from a verticle using the Java API:
- *
- * [source,javascript]
- * ----
- * console.log(java.lang.System.getProperty("foo"));
- * console.log(java.lang.System.getenv("HOME"));
- * ----
- *
  */
 @Document(fileName = "override/verticle-configuration.adoc")
 package docoverride.configuration;
