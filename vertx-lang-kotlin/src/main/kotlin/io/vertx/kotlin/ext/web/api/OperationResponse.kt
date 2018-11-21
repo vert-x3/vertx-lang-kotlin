e0c058b5ec2600d3174beb38fe7f94156ca90ebf
francesco <francescoguard@gmail.com>
2018-11-21 16:18:04 +0100

    Added vertx-web-api-service to code generation

---
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/ext/web/api/OperationResponse.kt
@@ -0,0 +1,24 @@
+package io.vertx.kotlin.ext.web.api
+
+import io.vertx.ext.web.api.OperationResponse
+
+fun OperationResponse(
+  headers: io.vertx.core.MultiMap? = null,
+  payload: io.vertx.core.buffer.Buffer? = null,
+  statusCode: Int? = null,
+  statusMessage: String? = null): OperationResponse = io.vertx.ext.web.api.OperationResponse().apply {
+
+  if (headers != null) {
+    this.setHeaders(headers)
+  }
+  if (payload != null) {
+    this.setPayload(payload)
+  }
+  if (statusCode != null) {
+    this.setStatusCode(statusCode)
+  }
+  if (statusMessage != null) {
+    this.setStatusMessage(statusMessage)
+  }
+}
+
