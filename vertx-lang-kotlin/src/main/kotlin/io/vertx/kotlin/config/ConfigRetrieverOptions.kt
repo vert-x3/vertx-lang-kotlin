d49434cdba3e26d855b110e2472b0b82618bd326
Julien Viet <julien@julienviet.com>
2018-11-29 09:20:47 +0100

    Releasing 4.0.0-SNAPSHOT

--- vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/config/ConfigRetrieverOptions.kt
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/config/ConfigRetrieverOptions.kt
@@ -6,10 +6,11 @@ import io.vertx.config.ConfigStoreOptions
 /**
  * A function providing a DSL for building [io.vertx.config.ConfigRetrieverOptions] objects.
  *
+ * Options to configure the <code>ConfigRetriever</code>.
  *
- * @param includeDefaultStores 
- * @param scanPeriod 
- * @param stores 
+ * @param includeDefaultStores  Enables or disables the inclusion of the default stored in the configuration.
+ * @param scanPeriod  Configures the scan period, in ms. This is the time amount between two checks of the configuration updates.
+ * @param stores  Sets the configuration stores.
  *
  * <p/>
  * NOTE: This function has been automatically generated from the [io.vertx.config.ConfigRetrieverOptions original] using Vert.x codegen.
--- vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/ext/web/api/contract/RouterFactoryOptions.kt
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/ext/web/api/contract/RouterFactoryOptions.kt
@@ -6,10 +6,9 @@ import io.vertx.ext.web.api.contract.RouterFactoryOptions
  * A function providing a DSL for building [io.vertx.ext.web.api.contract.RouterFactoryOptions] objects.
  *
  *
- * @param bodyHandler  Supply your own BodyHandler if you would like to control body limit, uploads directory and deletion of uploaded files
- * @param mountNotImplementedHandler  Automatic mount handlers that return HTTP 501 status code for operations where you didn't specify an handler.
+ * @param mountNotImplementedHandler  Automatic mount handlers that return HTTP 501 status code for operations where you didn't specify an handler. You can change the "not implemented handler" with [io.vertx.ext.web.api.contract.RouterFactory]
  * @param mountResponseContentTypeHandler  If true, when required, the factory will mount a [io.vertx.ext.web.handler.ResponseContentTypeHandler]
- * @param mountValidationFailureHandler  Enable or disable validation failure handler. If you enable it during router creation a failure handler that manages ValidationException will be mounted. You can change the validation failure handler with with function [io.vertx.ext.web.api.contract.RouterFactoryOptions]. If failure is different from ValidationException, next failure handler will be called.
+ * @param mountValidationFailureHandler  Enable or disable validation failure handler. If you enable it during router creation a failure handler that manages ValidationException will be mounted. You can change the validation failure handler with with function [io.vertx.ext.web.api.contract.RouterFactory]. If failure is different from ValidationException, next failure handler will be called.
  * @param operationModelKey  When set, an additional handler will be created to expose the operation model in the routing context under the given key. When the key is null, the handler is not added.
  * @param requireSecurityHandlers  If true, when you call [io.vertx.ext.web.api.contract.RouterFactory] the factory will mount for every path the required security handlers and, if a security handler is not defined, it throws an [io.vertx.ext.web.api.contract.RouterFactoryException]
  *
@@ -17,16 +16,12 @@ import io.vertx.ext.web.api.contract.RouterFactoryOptions
  * NOTE: This function has been automatically generated from the [io.vertx.ext.web.api.contract.RouterFactoryOptions original] using Vert.x codegen.
  */
 fun RouterFactoryOptions(
-  bodyHandler: io.vertx.ext.web.handler.BodyHandler? = null,
   mountNotImplementedHandler: Boolean? = null,
   mountResponseContentTypeHandler: Boolean? = null,
   mountValidationFailureHandler: Boolean? = null,
   operationModelKey: String? = null,
   requireSecurityHandlers: Boolean? = null): RouterFactoryOptions = io.vertx.ext.web.api.contract.RouterFactoryOptions().apply {
 
-  if (bodyHandler != null) {
-    this.setBodyHandler(bodyHandler)
-  }
   if (mountNotImplementedHandler != null) {
     this.setMountNotImplementedHandler(mountNotImplementedHandler)
   }
