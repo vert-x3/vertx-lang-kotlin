393629d7119b6b989a4329d7f4ebdff3a736465e
okou19900722 <ilayan19900722@gmail.com>
2018-12-18 17:23:25 +0800

    fix #105
    Signed-off-by: okou19900722 <ilayan19900722@gmail.com>

--- vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/cassandra/CassandraClient.kt
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/cassandra/CassandraClient.kt
@@ -1,5 +1,8 @@
 package io.vertx.kotlin.cassandra
 
+import com.datastax.driver.core.PreparedStatement
+import com.datastax.driver.core.Row
+import com.datastax.driver.core.Statement
 import io.vertx.cassandra.CassandraClient
 import io.vertx.cassandra.CassandraRowStream
 import io.vertx.cassandra.ResultSet
@@ -73,5 +76,75 @@ suspend fun CassandraClient.queryStreamAwait(sql : String) : CassandraRowStream 
 suspend fun CassandraClient.disconnectAwait() : Unit {
   return awaitResult{
     this.disconnect({ ar -> it.handle(ar.mapEmpty()) })}
+}
+
+/**
+ * Execute the query and provide a handler for consuming results.
+ *
+ * @param query the query to execute
+ * @returncurrent Cassandra client instance *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.CassandraClient original] using Vert.x codegen.
+ */
+suspend fun CassandraClient.executeWithFullFetchAwait(query : String) : List<Row> {
+  return awaitResult{
+    this.executeWithFullFetch(query, it)
+  }
+}
+
+/**
+ * Execute the query and provide a handler for consuming results.
+ *
+ * @param statement the statement to execute
+ * @returncurrent Cassandra client instance *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.CassandraClient original] using Vert.x codegen.
+ */
+suspend fun CassandraClient.executeWithFullFetchAwait(statement : Statement) : List<Row> {
+  return awaitResult{
+    this.executeWithFullFetch(statement, it)
+  }
+}
+
+/**
+ * Execute the statement and provide a handler for consuming results.
+ *
+ * @param statement the statement to execute
+ * @returncurrent Cassandra client instance *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.CassandraClient original] using Vert.x codegen.
+ */
+suspend fun CassandraClient.executeAwait(statement : Statement) : ResultSet {
+  return awaitResult{
+    this.execute(statement, it)
+  }
+}
+
+/**
+ * Prepares the provided query string.
+ *
+ * @param query the query to prepare
+ * @returncurrent Cassandra client instance *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.CassandraClient original] using Vert.x codegen.
+ */
+suspend fun CassandraClient.prepareAwait(query : String) : PreparedStatement {
+  return awaitResult{
+    this.prepare(query, it)
+  }
+}
+
+/**
+ * Executes the given SQL statement which returns the results of the query as a read stream.
+ *
+ * @param statement the statement to execute.
+ * @returncurrent Cassandra client instance *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.CassandraClient original] using Vert.x codegen.
+ */
+suspend fun CassandraClient.queryStreamAwait(statement : Statement) : CassandraRowStream {
+  return awaitResult{
+    this.queryStream(statement, it)
+  }
 }
 
--- vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/cassandra/ResultSet.kt
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/cassandra/ResultSet.kt
@@ -1,5 +1,6 @@
 package io.vertx.kotlin.cassandra
 
+import com.datastax.driver.core.Row
 import io.vertx.cassandra.ResultSet
 import io.vertx.kotlin.coroutines.awaitResult
 
@@ -13,5 +14,29 @@ import io.vertx.kotlin.coroutines.awaitResult
 suspend fun ResultSet.fetchMoreResultsAwait() : Unit {
   return awaitResult{
     this.fetchMoreResults({ ar -> it.handle(ar.mapEmpty()) })}
+}
+
+/**
+ *
+ * @return *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.ResultSet original] using Vert.x codegen.
+ */
+suspend fun ResultSet.oneAwait() : Row? {
+  return awaitResult{
+    this.one(it)
+  }
+}
+
+/**
+ *
+ * @return *
+ * <p/>
+ * NOTE: This function has been automatically generated from the [io.vertx.cassandra.ResultSet original] using Vert.x codegen.
+ */
+suspend fun ResultSet.allAwait() : List<Row> {
+  return awaitResult{
+    this.all(it)
+  }
 }
 
