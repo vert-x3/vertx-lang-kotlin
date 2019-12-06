68c8605aba3aef1102386ee31514d4d516f37b71
Julien Viet <julien@julienviet.com>
2019-12-06 15:10:12 +0100

    Releasing 4.0.0-SNAPSHOT

--- vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/ext/web/handler/BasicAuthHandler.kt
+++ vertx-lang-kotlin/src/main/kotlin/io/vertx/kotlin/ext/web/handler/BasicAuthHandler.kt
@@ -16,7 +16,6 @@
 package io.vertx.kotlin.ext.web.handler
 
 import io.vertx.core.json.JsonObject
-import io.vertx.ext.auth.User
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BasicAuthHandler
 import io.vertx.kotlin.coroutines.awaitResult
@@ -32,19 +31,6 @@ import io.vertx.kotlin.coroutines.awaitResult
 suspend fun BasicAuthHandler.parseCredentialsAwait(context: RoutingContext): JsonObject {
   return awaitResult {
     this.parseCredentials(context, it)
-  }
-}
-
-/**
- * Suspending version of method [io.vertx.ext.web.handler.BasicAuthHandler.authorize]
- *
- * @param user a user.
- *
- * NOTE: This function has been automatically generated from [io.vertx.ext.web.handler.BasicAuthHandler] using Vert.x codegen.
- */
-suspend fun BasicAuthHandler.authorizeAwait(user: User): Unit {
-  return awaitResult {
-    this.authorize(user, io.vertx.core.Handler { ar -> it.handle(ar.mapEmpty()) })
   }
 }
 
